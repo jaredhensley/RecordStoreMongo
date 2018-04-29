d3f8bceae571a9f63aa91598a970755580e65536
Jared Hensley <jahensle85@me.com>
2018-04-29 14:05:44 -0500

    model and schema updates

--- database/models/artist.js
+++ database/models/artist.js
@@ -1,1 +1,25 @@
-// Todo: Create Artist Model
+// Todo: create Album Schema
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+
+const ArtistSchema = new Schema({
+    name: String,
+    age: Number,
+    yearsActive: Number,
+    image: String,
+    genre: String,
+    website: String,
+    netWorth: Number,
+    labelName: String,
+    retired: Boolean,
+    albums: [{
+        type: Schema.Types.ObjectId,
+        ref: 'album'
+    }]
+});
+
+
+const Artist = mongoose.Model('artist', ArtistSchema);
+
+module.export = Artist;
